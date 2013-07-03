cc18a699009aa15d857101e6495b4c828c8ac939
Aaron Koop <aakoop@gmail.com>
2013-07-03 11:51:04 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 cp sublimetext2/* $HOME/.config/sublime-text-2/Packages/User/
 cp vim/.vimrc $HOME/
 
+# For vim plugin management
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
 # Let Vundle clone all plugins
 vim +BundleInstall +qall
