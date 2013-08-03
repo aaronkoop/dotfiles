309820d10c98d18b79d65cd1495d5e01678d4949
Aaron <aakoop@gmail.com>
2013-08-03 11:47:38 -0700

    adding zsh files to install script

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 ln -s ~/.dotfiles/sublimetext2/* ~/.config/sublime-text-2/Packages/User/
 ln -s ~/.dotfiles/vim/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/zsh/.zpreztorc ~/.zpreztorc
+ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 
 # For vim plugin management
 git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
