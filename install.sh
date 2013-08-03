1098da0569065a0bd7aed0a629374a4077814b40
Aaron <aakoop@gmail.com>
2013-08-03 12:06:37 -0700

    Adding tmux config to install script

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ ln -s ~/.dotfiles/sublimetext2/* ~/.config/sublime-text-2/Packages/User/
 ln -s ~/.dotfiles/vim/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/zsh/.zpreztorc ~/.zpreztorc
 ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
 
 # For vim plugin management
 git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
